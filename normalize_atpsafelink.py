574c76234eddab1db566e620c63fe1b47bc18802
Bobby Dennett <bobbydennett@users.noreply.github.com>
2021-03-12 01:36:26 -0800

    Fix dangling URL parameter

--- normalize_atpsafelink.py
+++ normalize_atpsafelink.py
@@ -4,8 +4,9 @@ from urllib import parse
 
 def normalize_atpsafelink(safelink_url):
     safelink_url = safelink_url.split("=")[1]
+    safelink_url = safelink_url.split('&')[0]
     safelink_url = parse.unquote(safelink_url)
-    safelink_url = safelink_url.split('&amp')[0]
+
     return safelink_url
 
 
@@ -15,7 +16,7 @@ def main(url):
 
 if __name__ == "__main__":
     args = ArgumentParser()
-    args.add_argument('-u', '--url', help="Enter the URL.", dest='url', required=True)
+    args.add_argument('-u', '--url', help="Enter the Office365 ATP SafeLinks URL", dest='url', required=True)
     args = args.parse_args()
 
     main(args.url)
